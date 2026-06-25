b22a7691f2180dca63f7130f87c172122dc20b14
Aniket Chauhan <cvinesh70@gmail.com>
2026-06-25 17:41:39 +0530

    db switch

--- Taskflow-Backend/core/settings.py
+++ Taskflow-Backend/core/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
